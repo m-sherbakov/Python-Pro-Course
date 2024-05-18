fb51bdb1f5a31e406dac3d9353dc30c705a70ff6
SHERBAKOV <167635823+m-sherbakov@users.noreply.github.com>
2024-05-18 19:55:32 +0300

    Update main.py
    New commands:
    - $crypto
    - $nft
    - $duck

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from discord.ext import commands
 import random
 import matplotlib.pyplot as plt
 import bot_logic
+import os
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 
 bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
 
@@ -54,5 +55,35 @@ async def bitcoin(ctx):
     plt.savefig('bitcoin_price.png')
     await ctx.send(f"Цена bitcoin: {round(price_history[-1])}")
     await ctx.send(file=discord.File('bitcoin_price.png'))
+
+@bot.command()
+async def crypto(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def nft(ctx):
+    img_name_nft = random.choice(os.listdir('nft'))
+    if img_name_nft == "lega1.png" or img_name_nft == "lega2.png":
+        for i in range(5):
+            await ctx.send('Legendary NFT!')
+    with open(f'nft/{img_name_nft}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
 
 bot.run("///")
